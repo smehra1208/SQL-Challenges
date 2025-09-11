2baa7ab7f0c41cc1257585a48d75f15e3b8f7c7a
Sakshi Mehra <smehra1208@gmail.com>
2025-09-11 16:45:10 +0530

    Create Type of Triangle.sql

---
+++ HackerRank/Advanced Select/Type of Triangle.sql
@@ -0,0 +1,18 @@
+-- Challenge: Type of Triangle
+/*
+ Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
+ Equilateral: It's a triangle with  sides of equal length.
+ Isosceles: It's a triangle with  sides of equal length.
+ Scalene: It's a triangle with  sides of differing lengths.
+ Not A Triangle: The given values of A, B, and C don't form a triangle.
+*/
+-- https://www.hackerrank.com/challenges/what-type-of-triangle/problem
+
+SELECT 
+    CASE
+        WHEN (A+B<=C) OR (B+C<=A) OR (C+A<=B) then 'Not A Triangle'
+        WHEN A=B AND B=C then 'Equilateral'
+        WHEN A=B OR B=C OR C=A then 'Isosceles'  
+        ELSE 'Scalene'
+    END as Type_of_Triangle
+    FROM TRIANGLES;
