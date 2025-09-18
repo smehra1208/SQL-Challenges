7354089b9339b29f912b23e1b47d56c17a746de4
Sakshi Mehra <smehra1208@gmail.com>
2025-09-18 16:49:09 +0530

    Create admissions_db_queries.sql

---
+++ Practice SQL/admissions_db_queries.sql
@@ -0,0 +1,24 @@
+-- https://www.sql-practice.com/
+/* Admissions Table Information
+		patient_id				INT
+		admission_date			DATE
+		discharge_date			DATE
+		diagnosis				TEXT
+		attending_doctor_id		INT
+*/
+
+-- 1. Show the total number of admissions
+SELECT COUNT(*) FROM admissions;
+
+-- 2. Show all the columns from admissions where the patient was admitted and discharged on the same day.
+SELECT * FROM admissions
+	WHERE admission_date = discharge_date;
+
+-- 3.Show the patient id and the total number of admissions for patient_id 579.
+SELECT patient_id , COUNT(*) as total_admissions
+	FROM admissions
+WHERE patient_io = 579;
+
+
+
+
